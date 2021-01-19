5f0fa2ca4433b9b266ded974717f305bea217528
Fanrr <2514509399@qq.com>
2021-01-19 15:42:37 +0800

    tags组件完成添加和选中

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">{{ tag }}
+      </li>
     </ul>
   </div>
 
 </template>
 
-<script>
-export default {
-  name: 'Tags'
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
 
+@Component
+export default class Tags extends Vue {
+  @Prop(Array) dataSource: string[] | undefined;
+
+  selectedTags: string[] = [];
+
+  toggle (tag: string) {
+    const index = this.selectedTags.indexOf(tag)
+
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1)
+    } else {
+      this.selectedTags.push(tag)
+    }
+  }
+
+  create () {
+    const name = window.prompt('请输入标签名')
+    if (name === '') {
+      window.alert('标签名不能为空')
+    } else if (name === null) {
+
+    } else if (this.dataSource!.indexOf(name) >= 0) {
+      window.alert('标签名重复')
+    } else if (this.dataSource) {
+      this.$emit('update:dataSource', [...this.dataSource, name])
+    }
+  }
 }
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,20 +2,25 @@
   <Layout class-prefix="layout">
     <NumberPad/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source.sync="tags"/>
     <Types/>
   </Layout>
 </template>
 
 <script>
-
 import NumberPad from '@/components/Money/NumberPad'
 import Notes from '@/components/Money/Notes'
 import Tags from '@/components/Money/Tags'
 import Types from '@/components/Money/Types'
+
 export default {
   name: 'Money',
-  components: { Types, Tags, Notes, NumberPad }
+  components: { Types, Tags, Notes, NumberPad },
+  data () {
+    return {
+      tags: ['衣', '食', '住', '行']
+    }
+  }
 }
 </script>
 
