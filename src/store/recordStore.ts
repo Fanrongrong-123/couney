8259a765435e7dc4d9f1b603389045ead116983f
Fanrr <2514509399@qq.com>
2021-01-26 23:52:39 +0800

    融合recordListModel和recordStore

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,6 +1,24 @@
-import recordListModel from '@/models/recordListModel'
+import clone from '@/lib/clone'
 
-export default {
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => { return recordListModel.create(record) }
+const localStorageKeyName = 'recordList'
+let data: RecordItem[] | undefined
+
+const recordStore = {
+  recordList: data,
+  fetchRecords () {
+    data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[]
+    return data
+  },
+  saveRecords () {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data))
+  },
+  createRecord: (record: RecordItem) => {
+    const record2: RecordItem = clone(record) // 深拷贝做一个record的副本，避免传值互相影响
+    record2.createAt = new Date()
+    data && data.push(record2)
+    recordStore.saveRecords()
+  }
 }
+recordStore.fetchRecords()
+
+export default recordStore
